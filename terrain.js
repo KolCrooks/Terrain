25b7567068340686d39e316a30ac60bf15fc3460
Kol <kolcrooks10@gmail.com>
2018-05-15 19:49:05 -0400

    now faster!

--- terrain.js
+++ terrain.js
@@ -5,26 +5,27 @@
 var terrainNoiseMap
 var mapsize
 var move
+var maps
 var img
 function setup(){
-    mapsize = createVector(500,250)
-    move = createVector(-250,55,600)
+    mapsize = createVector(400,300)
+    move = createVector(-200,45,650)
     createCanvas(1200, 800, WEBGL);
     terrainNoiseMap = new SimplexNoise() //CreateNoiseMap
+    maps = genTerrain(mapsize)
 }
 
 var rotated = -170
 var counter = 0
 function draw(){
-    counter += 5
-    background(100)
-
+    counter += 10
+    background(147,112,219)
     fill(0);
     stroke(255)
     push()
     translate(move)
     rotateX(radians(rotated))
-    drawTerrain(genTerrain(mapsize))
+    drawTerrain(maps)
     pop()
 }
 
@@ -36,7 +37,7 @@ function draw(){
 function genTerrain(vec){
     let w = vec.x
     let h = vec.y
-    let d = 5
+    let d = 10
     let tcloth = [[],[]]
     //loop through 2D array and set the position of each createVector point
     for(var x = 0; x < w/d; x++){
@@ -45,7 +46,7 @@ function genTerrain(vec){
             tcloth[x][z] = {
                 "x":x*d,
                 "z":z*d,
-                "y":terrainNoiseMap.noise(x*d,z*d+counter)*10,
+                "y":0,
                 "vel":createVector(0,0,0)
             }
 
@@ -55,22 +56,22 @@ function genTerrain(vec){
 }
 
 function drawTerrain(terrain){
-
+    var m = 15
     for(var x = 0; x < terrain.length; x++){
         for(var z = 0; z < terrain[x].length; z++) {
             //Check to see if there is a point next to it
             try {
                 //Create 2 triangles
                 beginShape(TRIANGLE_STRIP);
-                vertex(terrain[x][z].x,terrain[x][z].y,terrain[x][z].z)
-                vertex(terrain[x + 1][z].x, terrain[x + 1][z].y, terrain[x + 1][z].z)
-                vertex(terrain[x][z + 1].x, terrain[x][z + 1].y, terrain[x][z + 1].z)
+                vertex(terrain[x][z].x,terrainNoiseMap.noise(terrain[x][z].x,terrain[x][z].z+counter)*m,terrain[x][z].z)
+                vertex(terrain[x + 1][z].x,terrainNoiseMap.noise(terrain[x+1][z].x,terrain[x+1][z].z+counter)*m, terrain[x + 1][z].z)
+                vertex(terrain[x][z + 1].x,terrainNoiseMap.noise(terrain[x][z+1].x,terrain[x][z+1].z+counter)*m, terrain[x][z + 1].z)
                 endShape(CLOSE);
 
                 beginShape(TRIANGLE_STRIP);
-                vertex(terrain[x + 1][z + 1].x, terrain[x + 1][z + 1].y, terrain[x + 1][z + 1].z)
-                vertex(terrain[x + 1][z].x, terrain[x + 1][z].y, terrain[x + 1][z].z)
-                vertex(terrain[x][z + 1].x, terrain[x][z + 1].y, terrain[x][z + 1].z)
+                vertex(terrain[x + 1][z + 1].x, terrainNoiseMap.noise(terrain[x+1][z+1].x,terrain[x+1][z+1].z+counter)*m, terrain[x + 1][z + 1].z)
+                vertex(terrain[x + 1][z].x, terrainNoiseMap.noise(terrain[x + 1][z].x,terrain[x + 1][z].z+counter)*m, terrain[x + 1][z].z)
+                vertex(terrain[x][z + 1].x, terrainNoiseMap.noise(terrain[x][z + 1].x,terrain[x][z + 1].z+counter)*m, terrain[x][z + 1].z)
                 endShape(CLOSE);
             }catch(e){}
         }
